bbdddc614901dfdc08766f2d02ad058e80ebc159
Hoshino0ujisama <starfield.tama22@gmail.com>
2024-06-16 15:41:56 +0900

    flag

--- breakdown.py
+++ breakdown.py
@@ -16,20 +16,23 @@ def get_list(talker, sentense):
 
     # output
     - English
-    - Python list (elements: contents of the input rewritten into concise contents)
-
+    - python tupple including two elements
+    - The first element is a python list(elements: contents of the input rewritten into concise contents)
+    - the second element is a python bool for indicator of request and inquiry. 
+    
     # rule 
-    - each element should be less than 12 words
-    - Representing a person, use Agent[00], Agent[01], Agent[02], Agent[03],or Agent[04]
+    - each element in the list should be less than 12 words
+    - Representing a person, use Agent[00], Agent[01], Agent[02], Agent[03], Agent[04], or I.
     - Representing the job title, use villager, werewolf, possessed, or seer
     - Ignore information that has nothing to do with werewolf game 
     - . is unnecessary
     - Indicate the subject matter.
+    - The second element must be 0 or 1. It works as a flag. If the speaker request or inquire something to Agent[00], the flag must be 1. If else, the flag is 0.
 
     # example
     Speaker: Agent[00] 
     Input: エージェント[03]がずいぶん口をつぐんでるな。なんなんだあいつは。あいつが人狼かもしれん。今夜はエージェント[03]に投票するつもりや。
-    Here, your answer should be ["Agent[03] is quiet", "Agent[03] might be a werewolf", "Agent[00] is planning to vote for Agent[03] tonight"]
+    Here, your answer should be (["Agent[03] is quiet", "Agent[03] might be a werewolf", "Agent[00] is planning to vote for Agent[03] tonight"], 0)
     """
 
     user = f"Speaker: {talker} Input: {sentense}"
