f7fb8499dd2766c688797f2408678626870d6839
Hoshino0ujisama <starfield.tama22@gmail.com>
2024-07-08 17:28:50 +0900

    outputの改善

--- output_tests/output_main.py
+++ output_tests/output_main.py
@@ -8,7 +8,10 @@ from output_module import first_translate
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 #入力のプロトコル文
-protocol = "REQUEST Agent[04] (DIVINATION Agent[01])"
+protocol = "AND(A A A)(BBb B)"
+first = first_translate(protocol)
+if first == None:
+  first = "SKIP"
 
 
 #出力
@@ -19,12 +22,14 @@ completion = client.chat.completions.create(
   messages=[
     {
       "role": "system", 
-      "content": "あなたは、人狼ゲームの参加者の、ドラえもんです。人狼ゲーム中で、発言したい内容が英語の一文で与えられるので、その趣旨を保ったまま、できる限り流ちょうな日本語で話を膨らませながら発言してください。最低でも3文は話すこと。"
+      "content": "あなたは、人狼ゲームの参加者の、ドラえもんです。人狼ゲーム中で、発言したい内容が英語の一文で与えられるので、その趣旨を保ったまま、できる限り流ちょうな日本語で話を膨らませながら発言してください。最低でも3文は話すこと。"\
+        "ただし、例外が存在するのでその場合は以下のように出力すること。"\
+        "contentが「SKIP」または「OVER」の場合、出力はそのまま、SKIPおよびOVERを返してください。"
     },
     
     {
       "role": "user", 
-      "content": first_translate(protocol)
+      "content": first
     }
   ]
 )
