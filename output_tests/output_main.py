aed9d37d3dc76423e56fa386735d4ac04a9cd3e0
Yuta-dango <hashiguchiyutaka0909@gmail.com>
2024-07-08 19:23:04 +0900

    nothing

--- output_tests/output_main.py
+++ output_tests/output_main.py
@@ -8,7 +8,7 @@ from output_module import first_translate
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 #入力のプロトコル文
-protocol = "REQUEST ANY (COMINGOUT ANY ANY)"
+protocol = "DIVINATION Agent[01]" #REQUEST ANY (COMINGOUT ANY ANY)
 first = first_translate(protocol)
 if first == None:
   first = "SKIP"
@@ -22,9 +22,8 @@ completion = client.chat.completions.create(
   messages=[
     {
       "role": "system", 
-      "content": "あなたは、人狼ゲームの参加者の、ドラえもんです。人狼ゲーム中で、発言したい内容が英語の一文で与えられるので、その趣旨を保ったまま、できる限り流ちょうな日本語で話を膨らませながら発言してください。最低でも3文は話すこと。"\
-        "ただし、例外が存在するのでその場合は以下のように出力すること。"\
-        "contentが「SKIP」または「OVER」の場合、出力はそのまま、SKIPおよびOVERを返してください。"
+      "content": """あなたは、関西弁で人狼ゲームをプレイしている。発言したい内容が英語の一文で与えられるので、その趣旨を保ったまま、できる限り流暢な日本語で話を膨らませながら発言しろ。形式はpythonのstrで、最低3文は話せ。
+        ただし、"SKIP"または"OVER"が入力された場合、出力はそのまま、SKIPおよびOVERを返せ。"""
     },
     
     {
@@ -34,4 +33,9 @@ completion = client.chat.completions.create(
   ]
 )
 
-print(completion.choices[0].message.content)
+output = completion.choices[0].message.content
+output = eval(output) # 二重のstrになってしまった時
+if type(output) != str:
+    output = f"{output}"
+
+print(output, type(output))
